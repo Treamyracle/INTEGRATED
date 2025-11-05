a114eee293092c7602f9a4a9aaa5b161834f0d53
arif <arifathayaharahap@gmail.com>
2025-11-05 13:43:15 +0700

    add: CV

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,14 +1,25 @@
 // src/components/CV.js
 import React from 'react';
 import Navbar from './Navbar';
+import '../css/cv.css'; // Kita akan buat file CSS ini
+
+import pdfFile from "../assets/images/CV.pdf";
 
 const CV = () => {
   return (
     <div>
       <Navbar />
-      <div style={{ paddingTop: '100px', textAlign: 'center' }}>
+      <div className="cv-container">
         <h1>CV Saya</h1>
-        <p>Halaman ini akan menampilkan CV Anda (mungkin PDF viewer atau link download).</p>
+        <p>Berikut adalah CV saya. Anda juga bisa mengunduhnya melalui tombol di viewer.</p>
+        
+        <div className="pdf-viewer-wrapper">
+          <iframe 
+            src={pdfFile}
+            title="CV Arif Athaya Harahap"
+            className="pdf-iframe"
+          />
+        </div>
       </div>
     </div>
   );
